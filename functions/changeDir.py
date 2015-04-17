0ab7fd94471edab486e86c6f5335c2b61d0bb406
Pascal Gollor <pascal@pgollor.de>
2015-04-17 10:41:49 +0200

    change licenses

--- functions/changeDir.py
+++ functions/changeDir.py
@@ -9,7 +9,7 @@
 # Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 3.0 Unported-Lizenz.<br>
 # Um eine Kopie der Lizenz zu sehen, besuchen Sie http://creativecommons.org/licenses/by-sa/3.0/.<br>
 # -- englisch version --<br>
-# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Germany License.<br>
+# This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Germany License.<br>
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to<br>
 # Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 
